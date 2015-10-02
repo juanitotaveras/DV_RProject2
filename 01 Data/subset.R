cc0aa6f08ebc03b6de712695c5fd91a84cebf034
juanitotaveras <juanito.taveras@gmail.com>
2015-10-02 11:07:26 -0500

    Added subset and summary

---
+++ 01 Data/subset.R
@@ -0,0 +1,2 @@
+df <- data.frame(fromJSON(getURL(URLencode('129.152.144.84:5001/rest/native/?query="select * from RESPIRATORY_INFECTIONS"'),httpheader=c(DB='jdbc:oracle:thin:@129.152.144.84:1521/PDBF15DV.usuniversi01134.oraclecloud.internal', USER='cs329e_jmt3686', PASS='orcl_jmt3686', MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE), ))
+subset(df)
